e1bfa65e2c12fb6e49e5276148ae8f17eed653f1
Niels Otterspoor <niels.otterspoor@gmail.com>
2023-06-14 14:42:47 +0200

    bump

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modulesDir: [
+    './node-modules'
+  ],
   build: {
     transpile: [
       '@fortawesome/vue-fontawesome',
