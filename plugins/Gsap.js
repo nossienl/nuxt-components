d46eafe66e374efe31656da6ce98abcad5b4d1ef
Niels Otterspoor <niels.otterspoor@gmail.com>
2023-06-14 15:26:20 +0200

    gsap

--- plugins/Gsap.js
+++ plugins/Gsap.js
@@ -1,7 +1,7 @@
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
-import { Draggable } from 'gsap/Draggable'
+import { gsap } from '@/node_modules/gsap'
+import { ScrollTrigger } from '@/node_modules/gsap/ScrollTrigger'
+import { ScrollToPlugin } from '@/node_modules/gsap/ScrollToPlugin'
+import { Draggable } from '@/node_modules/gsap/Draggable'
 
 export default defineNuxtPlugin((nuxtApp) => {
     if (process.client) {
