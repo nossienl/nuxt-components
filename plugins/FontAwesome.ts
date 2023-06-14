4a1602d88f31cdbaae57a3d012f943e0d36f5786
Niels Otterspoor <niels.otterspoor@gmail.com>
2023-06-14 14:48:03 +0200

    bump

--- plugins/FontAwesome.ts
+++ plugins/FontAwesome.ts
@@ -1,6 +1,6 @@
-import { library, config } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library, config } from '@/node_modules/@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@/node_modules/@fortawesome/vue-fontawesome'
+import { fab } from '@/node_modules/@fortawesome/free-brands-svg-icons'
 
 library.add(fab);
 
